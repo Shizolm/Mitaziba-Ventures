aa096bffbd815f0b3e0a4f3edee0656899104d11
Shizolm <nwaghachisom796@gmail.com>
2022-09-16 22:52:10 +0100

    commit message

--- assets/js/scrollreveal.js
+++ assets/js/scrollreveal.js
@@ -3,7 +3,7 @@ ScrollReveal({
     reset: false,
     distance: '300px',
     duration: 1000,
-    delay: 400
+    delay: 500
 });
 
 // TARGET ELEMENTS TO ANIMATE AND SPECIFY OPTIONS TO CREATE REVEAL ANIMATIONS
@@ -12,30 +12,23 @@ ScrollReveal().reveal('.home_img', { delay: 500, origin: 'bottom', rotate: { y: 
 ScrollReveal().reveal('.home_data', { delay: 500, origin: 'top' });
 ScrollReveal().reveal('.home_data a', { delay: 1000, origin: 'bottom', rotate: { y: 100, z: 100 } });
 
-ScrollReveal().reveal('.section_title', { delay: 500, origin: 'right' });
-ScrollReveal().reveal('.section_subtitle', { delay: 500, origin: 'left' });
-ScrollReveal().reveal('.about_img', { delay: 500, origin: 'left' });
-ScrollReveal().reveal('.about_description', { delay: 500, origin: 'right' });
+
+ScrollReveal().reveal('.about_img', { delay: 500, origin: 'top' });
+ScrollReveal().reveal('.about_description', { delay: 500, origin: 'bottom' });
 ScrollReveal().reveal('.about_buttons', { delay: 500, origin: 'bottom' });
 
 ScrollReveal().reveal('.skills_titles', { delay: 500, origin: 'left', interval: 200 });
 
-//ScrollReveal().reveal('.services_container', { delay: 500, origin: 'right', interval: 200, rotate: { y: 100, z: 100 } });
-ScrollReveal().reveal('.services_container span', { delay: 500, origin: 'right' });
+ScrollReveal().reveal('.services_container', { delay: 500, origin: 'top',rotate: { y: 300, z: 300 } });
 
 ScrollReveal().reveal('.portfolio_container', { delay: 500, origin: 'bottom' });
 
-ScrollReveal().reveal('.project_title', { delay: 500, origin: 'left' });
-ScrollReveal().reveal('.project_description', { delay: 500, origin: 'right' });
-
-ScrollReveal().reveal('.project_data a', { delay: 500, origin: 'left' });
-ScrollReveal().reveal('.project_img', { delay: 500, origin: 'right' });
-
-ScrollReveal().reveal('.contact_info', { delay: 500, origin: 'left'});
-ScrollReveal().reveal('.contact_form', { delay: 500, origin: 'right'});
+ScrollReveal().reveal('.project_description', { delay: 500, origin: 'top' });
+ScrollReveal().reveal('.project_data a', { delay: 500, origin: 'bottom' });
 
 
-ScrollReveal().reveal('.footer_bg div, .footer_bg p', { delay: 500, origin: 'bottom'});
+ScrollReveal().reveal('.contact_info', { delay: 500, origin: 'top'});
+ScrollReveal().reveal('.contact_form', { delay: 500, origin: 'bottom'});
 
 
 
